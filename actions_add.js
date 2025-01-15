0a3a5b288f1e5a8a575cc5d59b40998929512a61
Samuel Bruce <11494533+samuelbruce@users.noreply.github.com>
2025-01-15 16:38:02 +1100

    Create reZero action
    For a given tracklist, find the max number of digits in 'order'
    field and pad with zeroes to that many digits

--- actions_add.js
+++ actions_add.js
@@ -2,11 +2,90 @@
 actions.deZero = {
   title: 'De-zero',
   execute: async function () {
+    const supportedFields = ['order']; //, 'discNo', 'episode', 'season'];
+    var message = '';
+    
+    let tracklist = uitools.getSelectedTracklist();
+    await tracklist.whenLoaded();
+    
+    for (let i = 0; i < supportedFields.length; i++) {
+      let fieldID = supportedFields[i];
+      let fieldDef = uitools.tracklistFieldDefs[fieldID];
+      
+      await listAsyncForEach(tracklist, async (track, next) => {
+        //fieldVals.push(fieldDef.getValue(track));
+        next();
+      }, () => {
+        uitools.toastMessage.show('test', {
+          disableUndo: true
+        });
+      });
+    }
+  
+  }
+}
+
+actions.reZero = {
+  title: 'Re-zero',
+  execute: async function () {
+    const supportedFields = ['order']; //, 'discNo', 'episode', 'season'];
+    var message = '';
+    
+    function isNum(str) {
+      if (typeof str != 'string') return false
+      return !isNaN(str) && !isNaN(parseFloat(str)) 
+    }
+
+    function maxDigits(list) {
+      let max = 0;
+      for (let i = 0; i < list.length; i++) {
+        let str = list[i];
+        if (isNum(str)) {
+          let d = str.length;
+          if (d > max) {
+            max = d;
+          }
+        }
+      }
+      return max;
+    }
+    
+    let tracklist = uitools.getSelectedTracklist();
+    await tracklist.whenLoaded();
+    
+    for (let i = 0; i < supportedFields.length; i++) {
+      let digits = 1;
+      let fieldID = supportedFields[i];
+      let fieldDef = uitools.tracklistFieldDefs[fieldID];
+      let fieldVals = [];
+      
+      listForEach(tracklist, (track) => {
+        let val = fieldDef.getValue(track);
+        fieldVals.push(val);
+      }, () => {
+        digits = maxDigits(fieldVals);
+      });
+      
+      await listAsyncForEach(tracklist, async (track, next) => {
+        let oldVal = fieldDef.getValue(track);
+        let newVal = String(oldVal).padStart(digits, '0');
+        fieldDef.setValue(track, newVal, true);
+        next();
+      }, () => {
+      });
+    }
+  
   }
 }
 
 window._menuItems.editTags.action.submenu.push({
     action: actions.deZero,
     order: 200,
+    grouporder: 10
+});
+
+window._menuItems.editTags.action.submenu.push({
+    action: actions.reZero,
+    order: 201,
     grouporder: 10
 });
